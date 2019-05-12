caea9a1abb0d10d5bbe0941cbec15856b615fbbd
Victor Montanez <victormontanez.student@careerdevs.com>
2019-05-12 21:40:47 +0000

    lesson13

---
+++ myapp/myapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Ninjas from './Ninjas';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React App!</h1>
+      <p>Welcome !</p>
+      <Ninjas name="Ryu" age="25" belt="black"/>
+    </div>
+  );
+}
+
+export default App;
