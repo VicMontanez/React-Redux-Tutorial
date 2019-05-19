654023dd4be823c3663d034cfbdeee35b4946fd0
Victor Montanez <victormontanez.student@careerdevs.com>
2019-05-18 21:13:18 -0400

    lesson31.js

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,44 +1,48 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
+
 
 class Home extends Component {
     state = {
-        posts : []
+        posts: [  ]
     }
-    componentDidMount() {
+    componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(res => {
-                console.log(res)
+            .then(res =>{
+                // console.log(res)
                 this.setState({
-                    posts: res.data.slice(0,10)
-                })
+                    posts:res.data.slice(0,10)
+                 })
             })
     }
+    
+    
     render() {
-        const { posts } = this.state;
-        const postList = posts.length ? (
-            posts.map(post => {
-                return (
-                    <div className="post card" key={post.id}>
-                        <div className="card-content">
-                            <span className="card-title">{post.title}</span>
-                            <p>{post.body}</p>
-                        </div>
+        const { posts } = this.state
+        const postLists = posts.length ? (
+           posts.map(post => {
+               return (
+                   <div className="post card" key={post.id}>
+                    <div className="card-content">
+                        <Link to={'/' + post.id}>
+                        <span className="card-title">{post.title}</span>
+                        </Link>
+                        <p>{post.body}</p>
                     </div>
-                )
-            })
+                   </div>
+               )
+           })
         ) : (
-            <div className="center">No post yet</div>
+            <div className="center"> No posts yet</div>
         )
-            return (
-                <div className="container">
-                    <h4 className="center">Home</h4>
-                    <p>{postList}</p>
-                </div>
-            )
-        
+        return (
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postLists}
+            </div>
+        )
     }
-}
-
+};
 
 export default Home;
